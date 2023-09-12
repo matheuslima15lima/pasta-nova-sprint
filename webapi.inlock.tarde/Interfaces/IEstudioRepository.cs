2e68261c74c5d9a5819f184ed33eeab646fb3acf
Matheus <matheus.cavalcanti5@aluno.senai.br>
2023-09-12 17:24:24 -0300

    Adicionado nova Api

---
+++ webapi.inlock.tarde/Interfaces/IEstudioRepository.cs
@@ -0,0 +1,20 @@
+﻿using webapi.inlock.tarde.Domains;
+
+namespace webapi.inlock.tarde.Interfaces
+{
+    public interface IEstudioRepository
+    {
+        List<Estudio> Listar();
+
+        Estudio BuscarPorId(Guid Id);
+
+        void Cadastrar(Estudio estudio);
+
+        void Atualizar(Guid Id, Estudio estudio);
+
+        void Deletar(Guid Id);
+
+        List<Estudio> ListarComJogos();
+
+    }
+}
---
+++ webapi.inlock.tarde/Repositories/EstudioRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using webapi.inlock.tarde.Contexts;
+using webapi.inlock.tarde.Domains;
+using webapi.inlock.tarde.Interfaces;
+
+namespace webapi.inlock.tarde.Repositories
+{
+    public class EstudioRepository : IEstudioRepository
+    {
+        InLockContext ctx = new InLockContext();
+        public void Atualizar(Guid Id, Estudio estudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudio BuscarPorId(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Estudio estudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudio> Listar()
+        {
+            return ctx.Estudios.ToList(); 
+        }
+
+        public List<Estudio> ListarComJogos()
+        {
+            return ctx.Estudios.ToList().Include(ICollection<Jogo>);
+
+           
+        }
+    }
+}
